7fda124cfde4843296455b9f195e96069fda810a
Zeyecx <zeyecx@gmail.com>
2021-07-21 01:54:21 +0200

    Template

--- src/main.py
+++ src/main.py
@@ -1,2 +1,11 @@
-from backend import *
-from frontend import *
+import gui as g
+import core as c
+
+def main():
+  """ Main Programm """
+  g.run()
+  print(c.test)
+
+# Run main Programm
+if __name__ == '__main__':
+    main()
